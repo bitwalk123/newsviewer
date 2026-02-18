b175fc0a9d9511280c67c9fdf226d022ed2c5751
Fuhito Suguri <bitwalk@gmail.com>
2026-02-18 19:03:55 +0900

    Windows 対応の検討

--- funcs/utils.py
+++ funcs/utils.py
@@ -5,11 +5,14 @@ from PySide6.QtGui import QDesktopServices
 # パスを一元管理（定数として定義）
 LOCAL_PARSER_DIR = Path.home() / ".local/share/newsviewer/parsers"
 
+
 def get_parser_dir():
     """ローカルパーサーのディレクトリパスを返す（無ければ作成）"""
+    # TODO: Windows への対応を考慮するのであれば、ロジックを変更する必要あり！
     if not LOCAL_PARSER_DIR.exists():
         LOCAL_PARSER_DIR.mkdir(parents=True, exist_ok=True)
     return LOCAL_PARSER_DIR
+
 
 def open_local_parser_dir():
     """ディレクトリをシステムデフォルトのファイルブラウザで開く"""
